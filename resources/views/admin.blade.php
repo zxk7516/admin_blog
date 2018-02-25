35eed62aef4700ab15b3cf8793d6c2bf88b3fea3
zxk7516 <zxk7516@foxmail.com>
2018-02-25 15:44:51 +0800

    dd

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,6 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="/index.css">
+    <!-- Polyfills -->
+    <!--[if lt IE 10]>
+    <script src="https://as.alipayobjects.com/g/component/??console-polyfill/0.2.2/index.js,es5-shim/4.5.7/es5-shim.min.js,es5-shim/4.5.7/es5-sham.min.js,es6-shim/0.35.1/es6-sham.min.js,es6-shim/0.35.1/es6-shim.min.js,html5shiv/3.7.2/html5shiv.min.js,media-match/2.0.2/media.match.min.js"></script>
+    <![endif]-->
+    <script src="https://as.alipayobjects.com/g/component/??es6-shim/0.35.1/es6-sham.min.js,es6-shim/0.35.1/es6-shim.min.js"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.bootcss.com/antd/3.2.1/antd.min.css" rel="stylesheet">
+
     <title>Laravel</title>
 </head>
 <body>
@@ -13,4 +23,5 @@
 </div>
 </body>
 <script src="{{ asset('js/admin.js') }}"></script>
+
 </html>
